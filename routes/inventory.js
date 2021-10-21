36a374964adfbc1901246d0443a0fd834a1ecc87
elias <etorres@snowfalltech.com>
2021-10-21 17:02:15 -0500

    Inventory WIP

--- routes/inventory.js
+++ routes/inventory.js
@@ -29,7 +29,6 @@ router.get('/ledger/barcode/:barcode', getInventoryLedgerBySku, async (req, res)
     res.status(200).json(res.inventory_ledger)
 })
 
-
 // Get inventory ledger records by sku_id
 router.get('/ledger/sku/:sku_id', getInventoryLedgerBySku, async (req, res) => {
     res.status(200).json(res.inventory_ledger)
@@ -75,7 +74,7 @@ router.post('/', async (req, res) => {
     const insert_data = data.reduce((a, i) => [...a, Object.values(i)], [])
     try {
         const newInventory = await pool.query('INSERT INTO inventory (sku_id, qty_received, expiration_date) VALUES ? ON DUPLICATE KEY UPDATE qty_received = qty_received + VALUES(qty_received) ', [
-            insert_data
+            insert_data,
         ])
         await createReceiptLedgerRecord(insert_data, reason_code_id, supplier_id, created_by)
         res.status(201).json(newInventory)
@@ -109,16 +108,13 @@ router.post('/adjustment', async (req, res) => {
     const data = req.body
 
     try {
-        const updatedInventory = await pool.query('UPDATE inventory SET qty_adjusted = qty_adjusted + ? WHERE sku_id = ?', [
-            data.qty_adjusted, data.sku_id
-        ])
+        const updatedInventory = await pool.query('UPDATE inventory SET qty_adjusted = qty_adjusted + ? WHERE sku_id = ?', [data.qty_adjusted, data.sku_id])
         if (updatedInventory[0].affectedRows != 0) {
             await createAdjustmentLedgerRecord(data)
             res.status(201).json(updatedInventory)
             return
         }
-        res.status(404).json({ 'status': 404, 'Message': 'Not updated' })
-
+        res.status(404).json({ status: 404, Message: 'Not updated' })
     } catch (error) {
         res.status(500).json({ message: error.message })
         console.error(error.message)
@@ -144,7 +140,14 @@ async function createReceiptLedgerRecord(insert_data, reason_code_id, supplier_i
 async function createSaleLedgerRecord(qty_shipped, sku_id, reason_code_id, client, created_by) {
     folio = await getFolio()
     try {
-        await pool.query('INSERT INTO inventory_ledger (folio, reason_code_id, sku_id, qty, client_id, created_by) VALUES (?, ?, ?, ?, ?, ?)', [folio, reason_code_id, sku_id, qty_shipped, client, created_by])
+        await pool.query('INSERT INTO inventory_ledger (folio, reason_code_id, sku_id, qty, client_id, created_by) VALUES (?, ?, ?, ?, ?, ?)', [
+            folio,
+            reason_code_id,
+            sku_id,
+            qty_shipped,
+            client,
+            created_by,
+        ])
         setFolio()
     } catch (error) {
         console.error(error.message)
@@ -154,7 +157,14 @@ async function createSaleLedgerRecord(qty_shipped, sku_id, reason_code_id, clien
 async function createAdjustmentLedgerRecord(data) {
     folio = await getFolio()
     try {
-        await pool.query('INSERT INTO inventory_ledger (folio, reason_code_id, sku_id, qty, description, created_by) VALUES (?, ?, ?, ?, ?, ?)', [folio, data.reason_code_id, data.sku_id, data.qty_adjusted, data.description, data.created_by])
+        await pool.query('INSERT INTO inventory_ledger (folio, reason_code_id, sku_id, qty, description, created_by) VALUES (?, ?, ?, ?, ?, ?)', [
+            folio,
+            data.reason_code_id,
+            data.sku_id,
+            data.qty_adjusted,
+            data.description,
+            data.created_by,
+        ])
         setFolio()
     } catch (error) {
         console.error(error.message)
@@ -185,7 +195,9 @@ async function setFolio() {
 async function getInventoryByBarcode(req, res, next) {
     try {
         const { barcode } = req.params
-        const inventory = await pool.query('SELECT sku.*,  qty_received - qty_shipped + qty_adjusted AS qty_available FROM inventory JOIN sku ON (sku.id = inventory.sku_id) WHERE barcode = ?', [barcode])
+        const inventory = await pool.query('SELECT sku.*,  qty_received - qty_shipped + qty_adjusted AS qty_available FROM inventory JOIN sku ON (sku.id = inventory.sku_id) WHERE barcode = ?', [
+            barcode,
+        ])
         if (inventory[0].length === 0) return res.status(404).json({ message: 'Inventory not found', status: 404 })
 
         res.inventory = inventory[0][0]
@@ -205,7 +217,8 @@ async function getInventoryLedgerBySku(req, res, next) {
             data = req.params.sku_id
             where = 'sku_id'
         }
-        const inventory_ledger = await pool.query(`SELECT folio, rc.description AS movement_type, sku.description AS sku_description, barcode, qty, COALESCE(su.name, '') AS supplier, COALESCE(cl.name, '') AS client, COALESCE(il.description, '') AS description, us.name, il.created_at
+        const inventory_ledger = await pool.query(
+            `SELECT folio, rc.description AS movement_type, sku.description AS sku_description, barcode, qty, COALESCE(su.name, '') AS supplier, COALESCE(cl.name, '') AS client, COALESCE(il.description, '') AS description, us.name, il.created_at
                                                     FROM inventory_ledger il
                                                     JOIN reason_code rc ON (rc.id = il.reason_code_id)
                                                     JOIN user us ON (us.id = il.created_by)
@@ -213,7 +226,9 @@ async function getInventoryLedgerBySku(req, res, next) {
                                                     LEFT JOIN supplier su ON (su.id = il.supplier_id)
                                                     LEFT JOIN client cl ON (cl.id = il.client_id)
                                                     WHERE ${where} = ?
-                                                    ORDER BY il.created_at DESC`, data)
+                                                    ORDER BY il.created_at DESC`,
+            data
+        )
         if (inventory_ledger[0].length === 0) return res.status(404).json({ message: 'Inventory ledger not found', status: 404 })
         res.inventory_ledger = inventory_ledger[0]
         next()
