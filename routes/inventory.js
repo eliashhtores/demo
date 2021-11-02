83bd0e9edf1155559bc1f6dd5a644df719031c35
eliashhtores <33284999+eliashhtores@users.noreply.github.com>
2021-11-02 07:39:32 -0600

    More improvements to inventory endpoint

--- routes/inventory.js
+++ routes/inventory.js
@@ -120,8 +120,8 @@ router.patch('/', async (req, res) => {
     let updatedInventory
     try {
         for (const element of data) {
-            updatedInventory += await pool.query('UPDATE inventory SET qty_shipped = qty_shipped + ? WHERE sku_id = ?', [element.qty_shipped, element.sku_id])
-            await createSaleLedgerRecord(element.qty_shipped, element.sku_id, reason_code_id, client_id, created_by)
+            updatedInventory += await pool.query('UPDATE inventory SET qty_shipped = qty_shipped + ? WHERE id = ?', [element.sku.qty_available, element.sku.inventory_id])
+            // await createSaleLedgerRecord(element.qty_available, element.inventory_id, reason_code_id, client_id, created_by)
         }
         res.status(201).json(updatedInventory)
     } catch (error) {
@@ -222,7 +222,7 @@ async function getInventoryByBarcode(req, res, next) {
     try {
         const { barcode } = req.params
         const inventory = await pool.query(
-            'SELECT sku.*,  (qty_received - qty_shipped + qty_adjusted) AS qty_available, DATE_FORMAT(expiration_date, "%Y-%m-%d") AS expiration_date FROM inventory JOIN sku ON (sku.id = inventory.sku_id) WHERE barcode = ? GROUP BY expiration_date ORDER BY expiration_date ',
+            'SELECT sku.*,  (qty_received - qty_shipped + qty_adjusted) AS qty_available, DATE_FORMAT(expiration_date, "%Y-%m-%d") AS expiration_date, inventory.id AS inventory_id FROM inventory JOIN sku ON (sku.id = inventory.sku_id) WHERE barcode = ? GROUP BY expiration_date ORDER BY expiration_date ',
             [barcode]
         )
         if (inventory[0].length === 0) return res.status(404).json({ message: 'Inventory not found', status: 404 })
